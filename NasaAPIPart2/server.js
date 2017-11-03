806f0bb6477960c8b0cfd24d88ed5ad54e2c8de0
ALEX MOLL <alexmoll@ALEXs-MacBook-Pro.local>
2017-11-03 08:43:56 -0600

    new updates

--- NasaAPIPart2/server.js
+++ NasaAPIPart2/server.js
@@ -1,6 +1,6 @@
 //server.js
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 // for every file inside this folder, makes a app.get route
 app.use(express.static('./public'))
@@ -10,24 +10,24 @@ const request = require('request');
 
 //req.query is query stream coming from the client
 app.get('/api', function(req, res) {
-  console.log(req.query)
+  console.log(req.query);
   var nasaUrl = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${req.query.start_date}&end_date=${req.query.end_date}&api_key=nxDNl3KDNp3hoakhRWDKErj3kIbrw8SS5Y7UsCQi`;
   request(nasaUrl, function(err, response, body) {
     console.log("started");
     //console.log(body)
-    res.send(body)
+    res.send(body);
   })
 })
 
 app.get('/', function(request, response) {
   response.sendFile('./public/index.html', {root: './'})
-  console.log("Welcome to the internet!")
+  console.log("Welcome to the internet!");
 })
 
 app.use(express.static('public'))
 
 app.get('/hello', function(request, response) {
-  response.send("Hello World")
+  response.send("Hello World");
 })
 
 app.get('/Home', function(request, response) {
@@ -45,9 +45,9 @@ app.get('/foo/bar', function(request, response) {
 app.post('/form-submit', function(request, response) {
   console.log("received post request!")
   //response.send("received the post request!")
-  response.redirect('/about')
+  response.redirect('/about');
 })
 
 app.listen(8080, function() {
-  console.log('The app is running on 8080')
+  console.log('The app is running on 8080');
 })
